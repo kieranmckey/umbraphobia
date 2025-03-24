f37372925df11661467d4c7e1e444f62c12dd2f6
Kieran Mckey <km319063@falmouth.ac.uk>
2025-03-24 22:10:46 +0000

    emitter

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -5,7 +5,10 @@ const SPEED = 40.0
 
 @onready var mesh = $MeshInstance3D
 @onready var ray = $RayCast3D
-@onready var particles = $GPUParticles3D
+#@onready var particles = $GPUParticles3D
+@onready var particles = $VfxOne
+@onready var explosion = $VfxOne/GPUParticles3D
+
 
 var player = null
 
@@ -16,22 +19,24 @@ var player = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_node(player_path)
+	particles.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += transform.basis * Vector3(0, 0, -SPEED) * delta
 	if ray.is_colliding():
-		mesh.visible = false
-		particles.emitting = true
+		mesh.visible = false		
 		ray.enabled = false
+		particles.visible = true
+		explosion.emitting = true
 		if ray.get_collider().is_in_group("enemy"):
 			ray.get_collider().hit()
 		if ray.get_collider().is_in_group("player"):
 			var dir = global_position.direction_to(player.global_position)
 			ray.get_collider().hit_player(dir)	
 		if get_tree():	
-			await get_tree().create_timer(1.0).timeout
+			await get_tree().create_timer(0.1).timeout
 		queue_free()
 
 
